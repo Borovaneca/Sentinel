35218fd09202d73d003249caa8e458665349f415
Borovaneca <petyoveselinov@gmail.com>
2024-09-13 20:17:55 +0300

    Adding endpoint for getting info from the db for backup during the migration.

--- src/main/java/bg/mck/sentinel/listeners/messages/RetrieveDB.java
+++ src/main/java/bg/mck/sentinel/listeners/messages/RetrieveDB.java
@@ -2,6 +2,8 @@ package bg.mck.sentinel.listeners.messages;
 
 import bg.mck.sentinel.reposotories.GoodSiteRepository;
 import bg.mck.sentinel.reposotories.SubDomainRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -13,11 +15,13 @@ public class RetrieveDB extends ListenerAdapter {
 
     private final GoodSiteRepository goodSiteRepository;
     private final SubDomainRepository subDomainRepository;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public RetrieveDB(GoodSiteRepository goodSiteRepository, SubDomainRepository subDomainRepository) {
+    public RetrieveDB(GoodSiteRepository goodSiteRepository, SubDomainRepository subDomainRepository, ObjectMapper objectMapper) {
         this.goodSiteRepository = goodSiteRepository;
         this.subDomainRepository = subDomainRepository;
+        this.objectMapper = objectMapper;
     }
 
 
@@ -30,13 +34,23 @@ public class RetrieveDB extends ListenerAdapter {
         if (contentRaw.equals("domains")) {
            event.getAuthor().openPrivateChannel()
                     .queue((privateChannel -> {
-                        privateChannel.sendMessage("`" + goodSiteRepository.findAll() + "`");
+                        try {
+                            String jsonResponse = objectMapper.writeValueAsString(goodSiteRepository.findAll());
+                            privateChannel.sendMessage("`" + jsonResponse + "`").queue();
+                        } catch (JsonProcessingException e) {
+                            privateChannel.sendMessage("Error processing JSON").queue();
+                        }
                     }));
 
         } else if (contentRaw.equals("subdomains")) {
             event.getAuthor().openPrivateChannel()
                     .queue((privateChannel -> {
-                        privateChannel.sendMessage("`" + subDomainRepository.findAll() + "`");
+                        try {
+                            String jsonResponse = objectMapper.writeValueAsString(subDomainRepository.findAll());
+                            privateChannel.sendMessage("`" + jsonResponse + "`").queue();
+                        } catch (JsonProcessingException e) {
+                            privateChannel.sendMessage("Error processing JSON").queue();
+                        }
                     }));
         }
     }
